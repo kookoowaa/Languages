b91fd92b1628560b79170eca2ffdfb3cf1c12200
Park, Pablo Chanwoo <kookoowaa@gmail.com>
2021-10-28 16:17:46 +0900

    Module and package

---
+++ 0.Python/Module/runModule.py
@@ -0,0 +1,8 @@
+from artithmetic_Module import sum as aSum
+
+from statistics_Module import sum as sSum
+
+print(aSum(4, 6))
+
+print(sSum(4,6))
+
---
+++ 0.Python/Module/runPackage.py
@@ -0,0 +1,8 @@
+from number_package.artithmetic_Module import sum as aSum
+
+from number_package.statistics_Module import sum as sSum
+
+print(aSum(4, 6))
+
+print(sSum(4,6))
+
