4775b9deadfc35f04c91b78911d70e4e76d3c7c3
Park, Pablo Chanwoo <kookoowaa@gmail.com>
2022-07-10 00:12:36 +0900

    Update Queries.sql

--- SQL for DA/Applied SQL for DA/Queries.sql
+++ SQL for DA/Applied SQL for DA/Queries.sql
@@ -1,5 +1,32 @@
 /*
-2022-07-09
+2022-07-10
 */
 
-select
+WITH
+  t1 AS (
+  SELECT
+    customer_id,
+    MIN(payment_date) AS first_payment,
+    SUM(amount) AS total_revenue
+  FROM
+    `jrjames83-1171.sampledata.payments`
+  GROUP BY
+    customer_id ),
+  t2 AS (
+  SELECT
+    customer_id,
+    amount,
+    ROW_NUMBER() OVER(PARTITION BY customer_id ORDER BY payment_date) AS nth_order
+  FROM
+    `jrjames83-1171.sampledata.payments` )
+
+
+SELECT
+  t1.customer_id, cast(t1.first_payment as date) as first_payment_date, round(t1.total_revenue, 2) as total_revenue, t2.amount as first_order_val
+FROM
+  t1
+LEFT JOIN
+  t2
+ON
+  t1.customer_id = t2.customer_id
+  AND t2.nth_order=1
