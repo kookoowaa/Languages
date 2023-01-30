d17fe6d0ea58f11c8bb7fd9679d071d3618d4df3
Park, Pablo Chanwoo <kookoowaa@gmail.com>
2023-01-31 01:11:03 +0900

    Update main.dart

--- Flutter and Dart/DART/main.dart
+++ Flutter and Dart/DART/main.dart
@@ -1,9 +1,39 @@
-void main() {
-var numberList = [1,2,3,1];
-var numberSet = {1,2,3,1};
-print(numberList);
-//[1,2,3,1]
-print(numberSet);
-//{1,2,3}
+class Player {
+  final String name;
+  int xp;
+  String team;
+
+  Player.fromJson(Map<String, dynamic> playerJson)
+      : name = playerJson['name'],
+        xp = playerJson['xp'],
+        team = playerJson['team'];
+
+  void sayHello(){
+    print("Hello this is $name");
+  }
 }
 
+void main() {
+  var apiData = [
+    {
+      "name": "PlayerA",
+      "team": "red",
+      "xp": 0,
+    },
+    {
+      "name": "PlayerB",
+      "team": "red",
+      "xp": 0,
+    },
+    {
+      "name": "PlayerC",
+      "team": "red",
+      "xp": 0,
+    },
+  ];
+
+  apiData.forEach((playerJson) {
+      var player = Player.fromJson(playerJson);
+      player.sayHello();
+   });
+}
